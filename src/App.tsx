595ac129632c5e2a2331462f2f4862eb6cbb226c
RoySegall <roy@segall.io>
2022-02-26 02:36:15 +0200

    Init stuff

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import {Word} from './Componenets/Word/Word';
+import styles from './App.module.scss'
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+import {useCallback, useEffect, useMemo, useState} from "react";
+
+function App() {
+  const selectedWord = 'שרשרת';
+  const allowedLetters = 'אבגדהוזחטיכלמנסעפצקרשתץםף'
+  const { width, height } = useWindowSize();
+  const [currentChance, setCurrentChance] = useState([]);
+  const [chances, setChances] = useState([]);
+  const [stopListenToKeyBoard, setStopListenToKeyBoard] = useState(false);
+  const won = useMemo(() => chances[chances.length -1] === selectedWord, [chances]);
+  const fillEmptyLines = useMemo(() => (currentChance.length !== 0 ? 4 : 5) - chances.length, [chances, currentChance]);
+
+  const handleKeyBoard = useCallback(({key}: KeyboardEvent) => {
+    if (key === 'Backspace') {
+      // Delete the words.
+      setCurrentChance([...currentChance.slice(0, -1)]);
+    }
+
+    if (key === 'Enter' && currentChance.length === 5) {
+      // Adding the current attempt, successful or not, to the list of chances.
+      let currentWord = currentChance.join('');
+      let tempChances = chances;
+      setCurrentChance([]);
+
+      if (currentWord === selectedWord || tempChances.length === 4) {
+        setStopListenToKeyBoard(true);
+      }
+
+      tempChances.push(currentWord);
+      setChances([...tempChances])
+    }
+
+    if (allowedLetters.includes(key) && currentChance.length !== 5) {
+      // The current letter is allowed so we can add it to the current chance.
+      currentChance.push(key);
+      setCurrentChance([...currentChance]);
+    }
+  }, [currentChance, setCurrentChance, chances]);
+
+  useEffect(() => {
+    addEventListener('keydown', handleKeyBoard);
+
+    if (stopListenToKeyBoard) {
+      // The user tried 5 times or got it the word correctly.
+      removeEventListener('keydown', handleKeyBoard);
+    }
+
+    return () => {
+      removeEventListener('keydown', handleKeyBoard);
+    };
+  }, [currentChance, stopListenToKeyBoard]);
+
+  return <div className={styles.app}>
+    <h1>סתוםלי</h1>
+
+    {won && <Confetti width={width} height={height} />}
+
+    {chances.map((chance, key) => <Word key={key} selectedWord={selectedWord} currentWord={chance} />)}
+    {currentChance && <Word selectedWord={selectedWord} currentWord={currentChance.join('')} currentChance={true} /> }
+    {[...Array.from({length: fillEmptyLines}).keys()]
+      .map((iterator, key) => <Word key={key} selectedWord={selectedWord} currentWord={''} optionalChance={true} />)}
+  </div>
+
+}
+
+export default App
---
+++ src/Componenets/Word/Word.tsx
@@ -0,0 +1,45 @@
+import styles from './Word.module.scss';
+
+interface WordProps {
+  selectedWord: string,
+  currentWord: string,
+  currentChance?: boolean
+  optionalChance?: boolean
+}
+
+function getColorClass(selectedWord: string, index: string, letter: string) {
+  if (selectedWord.includes(letter)) {
+    // @ts-ignore
+    return selectedWord.at(index) === letter ? 'correct' : 'wrongPlace';
+  }
+
+  return 'wrong';
+}
+
+const Word = ({selectedWord, currentWord, currentChance, optionalChance}: WordProps) => {
+
+  if (currentWord.length > 0 || optionalChance === true) {
+    // Calculating the number of spaces we need so we can have the extra empty boxes while typing.
+    let append = '';
+    for (let i = 0; i < 5 - currentWord.length; i++) {
+      append = `${append} `;
+    }
+
+    currentWord = `${currentWord}${append}`;
+  }
+
+  return <div className={styles.word}>
+    {Object.entries(currentWord).map(([index, letter]) => {
+      let locationClass = '';
+
+      if (!currentChance) {
+        // Check which class we need based on the letter position.
+        locationClass = currentWord.trim() ? getColorClass(selectedWord, index, letter) : '';
+      }
+      return <div key={index} className={`${styles.letter} ${styles[locationClass]}`}>{letter}</div>
+    })}
+  </div>
+};
+
+
+export { Word }
