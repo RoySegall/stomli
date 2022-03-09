6728d2cd835e52106701ec36b5a0f9263168ba96
RoySegall <roy@segall.io>
2022-03-09 22:00:06 +0200

    Adding the shuffles pages

---
+++ src/shuffle.ts
@@ -0,0 +1,20 @@
+import { words } from './words';
+import {shuffle, chunk, sample} from 'lodash'
+import {writeFile} from "fs";
+
+const shuffledWords = shuffle(words);
+const [firstYear, secondYear, thirdYear, leapYear, extra] = chunk(shuffledWords, 365);
+let randomWordFromExtra = sample(extra);
+
+if (randomWordFromExtra) {
+  leapYear.push(randomWordFromExtra);
+}
+
+writeFile('./shuffle-words.ts', JSON.stringify({
+  firstYear,
+  secondYear,
+  thirdYear,
+  leapYear
+}), (error) => {
+  console.error(error);
+})
