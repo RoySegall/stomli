1a08e8129361a622a075464c8c5a70d0683c75f7
RoySegall <roy@segall.io>
2022-03-09 22:12:49 +0200

    Adding some stuff

---
+++ src/wordsLogic.ts
@@ -0,0 +1,11 @@
+export function getTodayWord(): string {
+  return 'word';
+}
+
+export function getNextWord(usedWords: string[]): string {
+  return 'word';
+}
+
+export function checkIfWordIsValid(): boolean {
+  return false;
+}
