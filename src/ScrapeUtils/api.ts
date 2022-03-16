f17f1976aa61fd61bd5678ae8628e87519c42d52
RoySegall <roy@segall.io>
2022-03-16 08:52:49 +0200

    Commit work for now

--- src/ScrapeUtils/api.ts
+++ src/ScrapeUtils/api.ts
@@ -10,15 +10,3 @@ export async function openPuppeteerPage(url: string): Promise<{page: Page, brows
   return {page, browser};
 }
 
-export async function iterateOverLinks(page: Page, selector: string, itemProcessCallback: (anchor: any) => string) {
-  await page.waitForSelector(selector);
-
-  const links = await page.evaluate((selector) => {
-    console.log(document)
-    return Array.from(document.querySelectorAll(selector));
-
-    // return anchors.map(itemProcessCallback);
-  }, selector);
-
-  return links.map(itemProcessCallback)
-}
--- src/ScrapeUtils/songs.ts
+++ src/ScrapeUtils/songs.ts
@@ -1,22 +1,51 @@
-import {iterateOverLinks, openPuppeteerPage} from "./api";
+import {openPuppeteerPage} from "./api";
+import {uniq} from "lodash";
+import {Page} from "puppeteer";
 
-const pages = [
+const urls = [
   'https://shironet.mako.co.il/artist?type=works&lang=1&prfid=975',
   'https://shironet.mako.co.il/artist?type=works&lang=1&prfid=975&sort=alpha&class=1',
-  'https://shironet.mako.co.il/artist?type=works&lang=1&prfid=975&sort=alpha&class=1',
+  'https://shironet.mako.co.il/artist?type=works&lang=1&prfid=975&sort=alpha&class=2',
 ];
 
-async function buildUniquePages(): Promise<string[]> {
-  const {page, browser} = await openPuppeteerPage(pages[0]);
+async function scrapeNextPages(page: Page) {
+  try {
+    const results = await page.waitForXPath("//a[contains(., 'הבא') and @class='artist_nav_bar']", {timeout: 3000});
+    if (results) {
+      const nextPageLink = await (await results.getProperty('href')).jsonValue() as string;
+      await page.goto(nextPageLink);
+    }
+  } catch (e) {
+    console.log('Could not found the next page link');
+  }
+}
 
-  const links = await iterateOverLinks(page, 'a.artist_player_songlist', (anchor: any) => {
-    console.log(anchor.textContent);
-    return 'a';
-  });
+async function scrapePage(url: string, links: string[]) {
+  const {page, browser} = await openPuppeteerPage(url);
+  console.log('scraping ', url)
+  const selector = 'a.artist_player_songlist';
 
-  console.log(links)
+  const linksFromCurrentPage = await page.evaluate((selector) => {
+    const anchors = Array.from(document.querySelectorAll(selector));
+    return anchors.map((anchor) => {
+      return anchor.href
+    });
+  }, selector);
+
   await browser.close();
-  return [];
+
+  return [...links, ...linksFromCurrentPage];
+}
+
+async function buildUniquePages(): Promise<string[]> {
+  let links = [''];
+
+  for await (let url of urls) {
+    const linksFromPage = await scrapePage(url, links);
+    links = [...linksFromPage, ...links]
+  }
+
+  return uniq(links.filter(item => item.includes('wrkid')));
 }
 
 export async function scrape() {
